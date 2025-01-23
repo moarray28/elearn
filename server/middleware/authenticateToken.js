41b10175b2ab7a502d4a7a0f7924b8dd3e947a4f
moarray28 <moreritesh028@gmail.com>
2025-01-23 22:34:27 +0530

    adding jwt and cookie-parser

--- server/middleware/authenticateToken.js
+++ server/middleware/authenticateToken.js
@@ -12,7 +12,7 @@ const authenticateToken = (req, res, next) => {
       return res.status(403).json({ message: 'Invalid or expired token.' });
     }
 
-    req.user = user; // Attach user info to the request
+    req.user = user; 
     next();
   });
 };
